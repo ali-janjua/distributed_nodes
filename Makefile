7f2aed5d0f11504d6c004a4ef9e973724864baab
Ali Janjua <alinasir555@gmail.com>
2022-01-13 14:25:02 +0500

    Basic node connectivity

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
-build:
+build-code:
 	docker-compose run build_node sh -c "cd distributed_nodes && cargo build --release"
 
+build-container:
+	cd distributed_nodes && docker build -t distributed_node .
+
+build: build-code build-container
 clean:
 	rm -rf distributed_nodes/target
 	rm -rf distributed_nodes/Cargo.lock
 
-all: clean build
+all: build
--- distributed_nodes/src/main.rs
+++ distributed_nodes/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::{thread, time};
+use tokio::net::{TcpListener, TcpStream};
+
+#[tokio::main]
+async fn main() {
+    let nodes = env::var("NODES").unwrap_or("".to_string());
+    let split = nodes.split(" ");
+
+    let listener = TcpListener::bind("0.0.0.0:6379").await.unwrap();
+    println!("Listening...");
+    let listen_task_handle = tokio::spawn(async move {
+        loop {
+            let (_socket, _) = listener.accept().await.unwrap();
+            println!("Accepted");
+        }
+    });
+
+    for s in split {
+        println!("Connecting to peer: {}", s);
+        thread::sleep(time::Duration::from_secs(5));
+        let _socket = TcpStream::connect(&s).await;
+        println!("Connected!");
+    }
+    listen_task_handle.await.unwrap();
 }
