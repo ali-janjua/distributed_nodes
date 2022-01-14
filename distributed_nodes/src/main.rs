fc68fe794ba49b3de6cf44f6bc16217333f1e737
Ali Janjua <alinasir555@gmail.com>
2022-01-14 15:09:47 +0500

    2 way connectivity using alias and handshake of nodes

--- distributed_nodes/src/main.rs
+++ distributed_nodes/src/main.rs
@@ -5,22 +5,26 @@ use tokio::net::{TcpListener, TcpStream};
 #[tokio::main]
 async fn main() {
     let nodes = env::var("NODES").unwrap_or("".to_string());
-    let split = nodes.split(" ");
-
-    let listener = TcpListener::bind("0.0.0.0:6379").await.unwrap();
-    println!("Listening...");
+    let bind_addr = env::var("BIND").unwrap_or("0.0.0.0:6379".to_string());
     let listen_task_handle = tokio::spawn(async move {
+        let listener = TcpListener::bind(&bind_addr).await.unwrap();
+        println!("Listening to {}...", bind_addr);
         loop {
             let (_socket, _) = listener.accept().await.unwrap();
             println!("Accepted");
         }
     });
 
-    for s in split {
-        println!("Connecting to peer: {}", s);
-        thread::sleep(time::Duration::from_secs(5));
-        let _socket = TcpStream::connect(&s).await;
-        println!("Connected!");
-    }
+    let client_handle = tokio::spawn(async move {
+        let split = nodes.split(",");
+        for s in split {
+            println!("Connecting to peer: {}", s);
+            let _socket = TcpStream::connect(s).await.unwrap();
+            println!("Connected!");
+        }
+    });
+
     listen_task_handle.await.unwrap();
+    thread::sleep(time::Duration::from_secs(5));
+    client_handle.await.unwrap();
 }
